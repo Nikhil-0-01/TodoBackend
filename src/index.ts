be33f9a57b6bc5429b5d3b46c8600e6956da33b7
Nikhil <solankinikhil065@gmail.com>
2024-12-25 11:34:09 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -180,6 +180,31 @@ app.delete("/api/deleteTodo", Middleware, async (req, res) => {
   }
 });
 
+
+app.post('/api/createNote', Middleware, async (req: Request, res: Response) => {
+  const { title } = req.body;
+
+  try {
+    // const { userid } = req.body; 
+      const userid = req.userid;
+
+    if (!userid) {
+      return res.status(401).json({ message: 'Invalid Token' });  // Token validation failure
+    }
+
+    // Insert the new todo item into the database
+    const result = await pgClient.query(
+      `INSERT INTO Notes (title, user_id) VALUES ($1, $2) RETURNING *;`,
+      [title, userid]
+    );
+
+    res.json({ note : result.rows[0] });  // Send back the inserted todo
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });  // Handle any errors
+  }
+});
+
 // Listen to port (Vercel handles dynamic port assignment)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
